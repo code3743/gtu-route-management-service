1e618dc25ebc79a66f343a6ab15341b32b81b574
Esmeralda-RG <esmeralda.rivas@correounivalle.edu.co>
2025-03-26 21:48:10 -0500

    Add RouteMapper for DTO to domain object conversion in CreateRouteUseCase

--- src/main/java/com/gtu/route_management_service/application/usecase/CreateRouteUseCase.java
+++ src/main/java/com/gtu/route_management_service/application/usecase/CreateRouteUseCase.java
@@ -1,40 +1,30 @@
 package com.gtu.route_management_service.application.usecase;
 
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gtu.route_management_service.application.dto.RouteDTO;
+import com.gtu.route_management_service.application.mapper.RouteMapper;
 import com.gtu.route_management_service.domain.model.Route;
 import com.gtu.route_management_service.domain.service.RouteService;
 
+@Service
 public class CreateRouteUseCase {
-    private final RouteService routeService;
 
-    public CreateRouteUseCase(RouteService routeService) {
+    private final RouteService routeService;
+    private final RouteMapper routeMapper; 
+
+    public CreateRouteUseCase(RouteService routeService, RouteMapper routeMapper) {
         this.routeService = routeService;
+        this.routeMapper = routeMapper;
     }
 
     @Transactional
     public RouteDTO execute(RouteDTO routeDTO) {
-        Route route = new Route(
-                routeDTO.getName(),
-                routeDTO.getStartTime(),
-                routeDTO.getEndTime(),
-                routeDTO.getNeighborhoods(),
-                routeDTO.getStopIds());
-
+        Route route = routeMapper.toDomain(routeDTO);
         routeService.validateRoute(route);
         Route savedRoute = routeService.saveRoute(route);
 
-        return toDTO(savedRoute);
-    }
-
-    private RouteDTO toDTO(Route route) {
-        return new RouteDTO(
-                route.getName(),
-                route.getDescription(),
-                route.getStartTime(),
-                route.getEndTime(),
-                route.getNeighborhoods(),
-                route.getStopIds());
+        return routeMapper.toDTO(savedRoute); 
     }
 }
