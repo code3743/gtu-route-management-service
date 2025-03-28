cc7b8c70692314aab95d31c9e7927b37474f1db9
Esmeralda-RG <esmeralda.rivas@correounivalle.edu.co>
2025-03-28 11:24:32 -0500

    Add Lombok @Setter annotation to StopEntity for improved encapsulation

--- src/main/java/com/gtu/route_management_service/infrastructure/persistence/entities/StopEntity.java
+++ src/main/java/com/gtu/route_management_service/infrastructure/persistence/entities/StopEntity.java
@@ -6,12 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "stops")
 @Getter
+@Setter
 @NoArgsConstructor 
 @AllArgsConstructor 
 public class StopEntity {
